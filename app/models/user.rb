4b1551257a82ca8c3e3a975fd0293e4550ffabad
Leonardo Almeida <leoalmeidasa@gmail.com>
2025-07-09 16:51:14 -0300

    validations for email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+            format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "formato de email inválido" }
+  validates :password, presence: true, length: { minimum: 8, maximum: 20 }, if: :password_required?
+  validates :password_confirmation, presence: true, if: :password_required?
+
+  before_save { self.email = email.downcase }
+
+  private
+  )
+  def password_required?
+    !persisted? || !password.nil? || !password_confirmation.nil?
+  end
+
   has_many :transactions
   has_many :categories
   has_many :budgets
